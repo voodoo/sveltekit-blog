8f0ecb1cdea18a7bdb5c10ffd5238b24093ea4f9
Paul Vudmaska <paul.vudmaska@gmail.com>
2021-07-25 12:19:46 -0500

    initted

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,30 @@
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-netlify';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
+export default {
 	preprocess: preprocess(),
-
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		adapter: adapter()
 	}
 };
 
-export default config;
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	// Consult https://github.com/sveltejs/svelte-preprocess
+// 	// for more information about preprocessors
+// 	preprocess: preprocess(),
+
+// 	kit: {
+// 		// hydrate the <div id="svelte"> element in src/app.html
+// 		target: '#svelte',
+// 		adapter: adapter({
+// 			// default options are shown
+// 			pages: 'build',
+// 			assets: 'build',
+// 			fallback: null
+// 		})		
+// 	}
+// };
+
+// export default config;
